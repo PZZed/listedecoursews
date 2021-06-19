8e33a91ac7a448707b1b252228ded8acb0eee518
Leslie Caron <lesliecaron@live.fr>
2021-06-19 18:17:57 +0200

    initial commit

---
+++ src/test/java/com/coloc/listedecourse/ListedecourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coloc.listedecourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListedecourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
